639d526c06f3a812c2f4d64ed944934e15611538
lvgoang <lvgang@univer.ai>
2018-12-17 14:58:05 +0800

    service-message工程上传
    message服务上传

---
+++ workspace/service-message/src/main/java/com/univer/message/ServiceMessageApplication.java
@@ -0,0 +1,16 @@
+package com.univer.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lvgang
+ * @descript
+ * @time 2018-12-17 11:52
+ */
+@SpringBootApplication
+public class ServiceMessageApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ServiceMessageApplication.class,args);
+    }
+}

605b8a5bd9282a7b763eb38964886af96f6b02ad
lvgoang <lvgang@univer.ai>
2018-12-19 21:01:42 +0800

    提交修改

--- workspace/service-course/src/main/java/com/univer/course/ServiceCourseApplication.java
+++ workspace/service-course/src/main/java/com/univer/course/ServiceCourseApplication.java
@@ -2,6 +2,7 @@ package com.univer.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author lvgang
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @time 2018-12-13 15:56
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.univer.course.mapper" )
 public class ServiceCourseApplication {
     public static void main(String[] args){
         SpringApplication.run(ServiceCourseApplication.class,args);

5c9f1c5b832bccd0f003f3cbd5efb7368e19d8f9
2637977081 <2637977081@qq.com>
2018-12-28 00:00:19 +0800

    ui设计

---
+++ UI/js/init.js
@@ -0,0 +1,21 @@
+
+var bashAccountIp = "127.0.0.1";
+var bashAccountPort = "8031";
+var bashStoragePort = "8041";
+
+//账号管理服接口调用
+var bashAccountUrl = "http://"+bashAccountIp+":"+bashAccountPort+"/account/";
+var bashUserUrl = "http://"+bashAccountIp+":"+bashAccountPort+"/user/";
+var bashOrgUrl = "http://"+bashAccountIp+":"+bashAccountPort+"/org/";
+var bashRoleUrl = "http://"+bashAccountIp+":"+bashAccountPort+"/role/";
+
+//存储服务接口调用
+var bashStorageUrl = "http://"+bashAccountIp+":"+bashStoragePort+"/file/";
+
+var user = null;
+
+var random = "0000";
+
+var nginxIp = "192.168.152.128";
+
+var user = null;
